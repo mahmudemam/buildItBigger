af06b4d8069441ee90cabadbd3f87048ec1dbc8b
Mahmoud Emam <mahmoud.emam@ashtrywla.com>
2018-06-21 20:07:18 +0200

    adds the java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.udacity.nd.projects.jokes.TellAJoke;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, new TellAJoke().getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ jokes/src/main/java/com/udacity/nd/projects/jokes/TellAJoke.java
@@ -0,0 +1,7 @@
+package com.udacity.nd.projects.jokes;
+
+public class TellAJoke {
+    public String getJoke() {
+        return "A funny joke";
+    }
+}
