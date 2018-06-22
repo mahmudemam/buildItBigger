dc911b28faf2f9384d02264795c85f6b24d907f4
Mahmoud Emam <mahmoud.emam@ashtrywla.com>
2018-06-22 02:21:25 +0200

    adds test case for asyncTask

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityInstrumentedTest.java
@@ -0,0 +1,36 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import junit.framework.Assert;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.ExecutionException;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityInstrumentedTest {
+    private static final String TAG = MainActivityInstrumentedTest.class.getSimpleName();
+    @Rule
+    public ActivityTestRule<MainActivity> mRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void executeAsyncTask_returnsNonEmptyString() {
+        try {
+            String result = new EndpointsAsyncTask(mRule.getActivity()).execute().get();
+
+            Log.i(TAG, result);
+
+            Assert.assertNotNull(result);
+            Assert.assertTrue(! result.isEmpty());
+        } catch (InterruptedException e) {
+            Log.e(TAG, e.getMessage());
+        } catch (ExecutionException e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+}
